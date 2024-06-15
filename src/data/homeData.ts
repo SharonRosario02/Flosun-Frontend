6b8b5d5e4659fbd96507b2791ffc30b3dd296f69
unknown <sharon2002222@gmail.com>
2024-06-15 17:02:28 +0530

    image filed updated to img in homeData

--- src/data/homeData.ts
+++ src/data/homeData.ts
@@ -4,19 +4,19 @@ import Img3 from "../img/care_svg.svg";
 
 export const homeData = [
   {
-    image: Img1,
+    img: Img1,
     alt: "Unbeatable quality image",
     title: "Unbeatable Quality",
     text: "We source directly from top-rated growers, ensuring our flower bouquets are crafted with the freshest and most beautiful blooms.",
   },
   {
-    image: Img2,
+    img: Img2,
     alt: "Delivery image",
     title: "Delivery to Your Door",
     text: "We deliver stunning bouquets straight to your doorstep, anywhere in LT. If you’re not 100% satisfied, let us know within 30 days and we'll make it right.",
   },
   {
-    image: Img3,
+    img: Img3,
     alt: "Help image",
     title: "Expert Assistance",
     text: "Our floral experts are here to help you with any questions or concerns. Plus, we offer a free guide on caring for your bouquets to ensure they last longer.",
